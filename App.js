9ace37b679c7b03ab86e76d0f9beb4d9d530d8fd
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-12-02 23:43:37 -0300

    Commit de prueba

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Tareas de hoy */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Tareas de hoy</Text>
+
+        <View style={styles.items}>
+          {/* Aqui van las tareas de hoy */}
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
+        </View>
+
+      </View>
+
     </View>
   );
 }
@@ -13,8 +25,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop: 90,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
   },
 });
